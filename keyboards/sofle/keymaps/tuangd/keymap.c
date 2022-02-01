0f638057491c59e3d651775ce0ccdb5c0b104209
tuangd <tuangd@gmail.com>
2022-02-02 00:15:40 +0700

    changing config.h in my kepmap folder

--- keyboards/sofle/keymaps/tuangd/keymap.c
+++ keyboards/sofle/keymaps/tuangd/keymap.c
@@ -1,20 +1,6 @@
 #include QMK_KEYBOARD_H
 
-#define MOUSEKEY_INTERVAL 8
 
-#define MOUSEKEY_DELAY 0
-
-#define MOUSEKEY_TIME_TO_MAX 1000
-
-#define MOUSEKEY_MAX_SPEED 9
-
-#define MOUSEKEY_WHEEL_DELAY 0
-
-#define MOUSEKEY_WHEEL_INTERVAL 100
-
-#define MOUSEKEY_WHEEL_TIME_TO MAX 80
-
-#define MOUSEKEY_WHEEL_MAX_SPEED 3
 
 
 enum sofle_layers {
@@ -52,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK] = LAYOUT(
   KC_F14,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_ENT,
-  KC_F13,  KC_A,   KC_S,    KC_D,LT(_ARROW,KC_F),  KC_G,                LT(_FUNC,KC_H),   KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_F13,  KC_A,   KC_S,    KC_D,LT(_ARROW,KC_F),  KC_G,                 KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_ESC,      TG(_QWRTY), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-		KC_LGUI,KC_LCTRL, KC_LALT, KC_SPC,  KC_DEL,      			  TG(_FUNC),LT(_MOUSE,KC_RBRC), KC_MINS, KC_EQL, KC_LBRC 
+		KC_LGUI,KC_LCTRL, KC_LALT, KC_SPC,  KC_DEL,      			  TT(_FUNC),LT(_MOUSE,KC_RBRC), KC_MINS, KC_EQL, KC_LBRC 
 ),
 /* Lower (1)
  * ,-----------------------------------------.                    ,-----------------------------------------.
