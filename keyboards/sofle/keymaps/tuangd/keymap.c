79421c630d57c2f85d9e5efbced87c747fc99193
tuangd <tuangd@gmail.com>
2022-02-06 11:01:38 +0700

    switch ctrl and window buttons

--- keyboards/sofle/keymaps/tuangd/keymap.c
+++ keyboards/sofle/keymaps/tuangd/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|  ESC  |    |  TG3  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LCTR | LALT |Space | /DEL    /       \ TG2  \  |LT2, ]|   -  | =    | [    |
+ *            | LCTR | LGUI | LALT |Space | /DEL    /       \ TG2  \  |LT2, ]|   -  | =    | [    |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -38,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK] = LAYOUT(
   KC_F14,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_ENT,
-  KC_F13,  KC_A,   KC_S,    KC_D,LT(_ARROW,KC_F),  KC_G,                 KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_F13,   KC_A,   KC_S,    KC_D,LT(_ARROW,KC_F),  KC_G,                KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_ESC,      TG(_QWRTY), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-		KC_LGUI,KC_LCTRL, KC_LALT, KC_SPC,  KC_DEL,      			  TT(_FUNC),LT(_MOUSE,KC_RBRC), KC_MINS, KC_EQL, KC_LBRC 
+	        KC_LCTRL, KC_LGUI, KC_LALT,  KC_SPC,  KC_DEL,    TT(_FUNC),LT(_MOUSE,KC_RBRC), KC_MINS, KC_EQL, KC_LBRC 
 ),
 /* Lower (1)
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -80,13 +80,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_MOUSE] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                          _______,  _______, _______,  _______,  _______,_______,
-  _______, KC_MS_L,  KC_MS_U,KC_MS_R, _______, _______,                          _______,  _______, _______,  _______,  _______,_______,
-  _______, _______,  KC_MS_D,_______, _______, _______,                          _______,  KC_BTN1, KC_BTN2,  KC_BTN3,  _______,_______,
+  _______, _______, _______, KC_MS_U, _______, _______,                          _______,  _______, _______,  _______,  _______,_______,
+  _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,                          _______,  KC_BTN1, KC_BTN2,  KC_BTN3,  _______,_______,
   _______, _______, _______, _______, _______, _______,  _______,      _______,  _______,  KC_WH_U, KC_WH_D,  _______,  _______,_______,
                     _______, _______, _______,  _______, _______,      _______, _______,   _______, _______, _______
 ),
 
-/*
+/*e
  * DVORAK to QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
