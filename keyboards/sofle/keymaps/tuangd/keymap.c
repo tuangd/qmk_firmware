1cb371f530ee082a1e8d5e8756faf525e385db15
tuangd <tuangd@gmail.com>
2022-03-17 17:27:13 +0700

    add tapDance and change ESC to PSCR

--- keyboards/sofle/keymaps/tuangd/keymap.c
+++ keyboards/sofle/keymaps/tuangd/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Enter|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |F13   |   A  |   S  |   D  |LT4,F |   G  |-------.    ,-------|LT1,H |   J  |   K  |   L  |   ;  |  '   |
- * |------+------+------+------+------+------|  ESC  |    |  TG3  |------+------+------+------+------+------|
+ * |------+------+------+------+------+------|PrnScrn|    |  TG3  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LCTR | LGUI | LALT |Space | /DEL    /       \ TG2  \  |LT2, ]|   -  | =    | [    |
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F14,   KC_1,   KC_2,    KC_3,    KC_4,TD(TD_5_ESC),                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_ENT,
   KC_F13,   KC_A,   KC_S,    KC_D,LT(_ARROW,KC_F),  KC_G,                KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_ESC,      TG(_QWRTY), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_PSCR,      TG(_QWRTY), KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
 	        KC_LCTRL, KC_LGUI, KC_LALT,  KC_SPC,  KC_DEL,    TT(_FUNC),LT(_MOUSE,KC_RBRC), KC_MINS, KC_EQL, KC_LBRC 
 ),
 /* Lower (1)
